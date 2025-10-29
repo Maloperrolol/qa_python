8645c9cece58360d7902db5a17b100c8142bd1f8
Kirill Astrakhov <kirill__astrakhov@mail.ru>
2025-10-29 22:28:25 +0300

    Добавил проверку наа получение жанра книги и исправвил тест на устновку жанра

--- tests.py
+++ tests.py
@@ -12,7 +12,7 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self, collector):
         # создаем экземпляр (объект) класса BooksCollector
         
 
@@ -28,24 +28,29 @@ class TestBooksCollector:
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
     #Проверить что ннельзя добавить книгу если название длиннее 40 символов
-    def test_add_new_book_name_too_long_not_added(self):
+    def test_add_new_book_name_too_long_not_added(self, collector):
         long_name = 'A' * 45
         collector.add_new_book(long_name)
         assert long_name not in collector.get_books_genre()
     #Проверить что книге можно задать жанр
-    def test_set_book_genre_valid_genre(self):
+    def test_set_book_genre_valid_genre(self, collector):
         collector.add_new_book('Гарри Поттер')
         collector.set_book_genre('Гарри Поттер', 'Фантастика')
+        assert collector.book_genre['Гарри Поттер'] == 'Фантастика'
+
+    #Проверка на получение жанра кнгиги
+    def test_get_book_genre_returns_correct_value(self, collector):
+        collector.books_genre = {'Гарри Поттер':'Фантастика'}
         assert collector.get_book_genre('Гарри Поттер') == 'Фантастика'
     #Проверка  что метод возвращает словарь с книгами
-    def test_get_books_genre_returns_dict(self):
+    def test_get_books_genre_returns_dict(self, collector):
         collector.add_new_book('Гарри Поттер')
         collector.set_book_genre('Гарри Поттер', 'Фантастика')
         books_genre = collector.get_books_genre()
 
         assert books_genre['Гарри Поттер'] == 'Фантастика'
     #Проверить что возвращается книга определенного жанра
-    def test_get_books_with_specific_genre_returrns_correct_books(self):
+    def test_get_books_with_specific_genre_returrns_correct_books(self, collector):
         collector.add_new_book('Гарри Поттер')
         collector.add_new_book('Оно')
         collector.set_book_genre('Гарри Поттер', 'Фантастика')
@@ -53,7 +58,7 @@ class TestBooksCollector:
         result = collector.get_books_with_specific_genre('Фантастика')
         assert result == ['Гарри Поттер']
     #Проверить что можно вернуть книги подходящие детям
-    def test_get_books_for_children_returns_only_child_friendly_books(self):
+    def test_get_books_for_children_returns_only_child_friendly_books(self, collector):
        collector.add_new_book('Алиса в стране чудес')
        collector.add_new_book('Сияние')
        collector.set_book_genre('Алиса в стране чудес', 'Мультфильмы')
@@ -61,18 +66,18 @@ class TestBooksCollector:
        result = collector.get_books_for_children()
        assert result == ['Алиса в стране чудес']
     #Проверить что книга добавляется в избранное
-    def test_add_books_in_favorites_adds_book(self):
+    def test_add_books_in_favorites_adds_book(self, collector):
         collector.add_new_book('Гарри Поттер')
         collector.add_book_in_favorites('Гарри Поттер')
         assert collector.get_list_of_favorites_books() == ['Гарри Поттер']
     #Проверить что книгу нельзя добавить в избраанное дважды
-    def test_add_book_in_favorites_does_not_diplicate(self):
+    def test_add_book_in_favorites_does_not_diplicate(self, collector):
         collector.add_new_book('Гарри Поттер')
         collector.add_book_in_favorites('Гарри Поттер')
         collector.add_book_in_favorites('Гарри Поттер')
         assert collector.get_list_of_favorites_books().count('Гарри Поттер') == 1
     #проверить что книгу можно удалить из избранного
-    def test_delete_book_from_favorites_removes_book(self):
+    def test_delete_book_from_favorites_removes_book(self,collector):
         collector.add_new_book('Гарри Поттер')
         collector.add_book_in_favorites('Гарри Поттер')
         collector.delete_book_from_favorites('Гарри Поттер')
@@ -88,7 +93,7 @@ class TestBooksCollector:
             ('Алиса в стране чудес', 'Мультфильмы'),
         ]
     )
-    def test_set_book_genre_with_parametrize(self, book_name, genre):
+    def test_set_book_genre_with_parametrize(self, collector, book_name, genre):
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, genre)
         assert collector.get_book_genre(book_name) == genre
